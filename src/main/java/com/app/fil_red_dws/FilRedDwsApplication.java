0fb9597b98ec4dd8e8e0bba3c549a733ea08c5b2
yassine ELANIQ <yassaniq@gmail.com>
2025-05-29 12:42:01 +0100

    initialisation du projet fil rouge

---
+++ src/main/java/com/app/fil_red_dws/FilRedDwsApplication.java
@@ -0,0 +1,13 @@
+package com.app.fil_red_dws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilRedDwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FilRedDwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/fil_red_dws/FilRedDwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.fil_red_dws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilRedDwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
